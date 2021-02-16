bc479f49eaa0811dd679ada047ba3ae7330b56c4
Tim de kok <de.kok.ac@gmail.com>
2021-02-16 13:51:19 +0100

    edited some things in EventModal.vue. now its time to test this

--- src/Http/Controllers/EventsController.php
+++ src/Http/Controllers/EventsController.php
@@ -23,7 +23,7 @@ class EventsController
 	        ->get(['est_id', 'address'])
             ->toJson();
 
-        return response($events, $projects, $estates);
+        return response([$events, $projects, $estates]);
     }
 
     public function store(Request $request)
